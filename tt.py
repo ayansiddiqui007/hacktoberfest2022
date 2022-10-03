743de6346f12894f6905cb7d09fb179dd65b7f43
Hack-stackOverflow <72503669+Hack-stackOverflow@users.noreply.github.com>
2022-10-03 11:18:51 -0700

    #3
    #3

---
+++ tt.py
@@ -0,0 +1,16 @@
+# Variable to keep the main loop running
+running = True
+
+# Main loop
+while running:
+    # Look at every event in the queue
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+
+        # Did the user click the window close button? If so, stop the loop.
+        elif event.type == QUIT:
+            running = False
